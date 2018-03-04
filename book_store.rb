5d528d66e0dd24b6e981ea3cf01309a46f2cf03d
c1505 <coreym247@gmail.com>
2018-03-03 21:22:08 -0500

    refactoring.  Created instance of book class to allow for storag of instance variables. Began to extract methods

--- book_store.rb
+++ book_store.rb
@@ -1,52 +1,24 @@
+require 'pry'
 class BookStore
-  def self.calculate_price(books)
-    counts = Hash.new(0)
-    books.each { |book| counts[book] += 1 }
-    number_of_groups = (books.length / 5.0).ceil
-    total = 0
+  attr_reader :books
 
+  def initialize(books)
+    @books = books
+  end
+
+  def self.calculate_price(books)
+    bookstore = self.new(books)
+    bookstore.calculate_price
+  end
+
+  def calculate_price
+    total = 0
     if number_of_groups == 1
-      total = self.book_group_price(books)
+      total = book_group_price(books)
     elsif number_of_groups > 1
-      group_1 = []
-      group_2 = []
-      group_3 = []
-      group_4 = []
-      switcher = false
-      counts.each do |k, v|
-        if v == 2
-          if group_1.length > 3 && books.length > 12
-            group_3 << k
-            group_4 << k
-          else
-            group_1 << k
-            group_2 << k
-          end
-        elsif v == 3
-          group_1 << k
-          group_2 << k
-          group_3 << k
-        elsif v == 4
-          group_1 << k
-          group_2 << k
-          group_3 << k
-          group_4 << k
-        elsif switcher == false
-          group_1 << k
-          switcher = books.length != 6
-        elsif switcher == true
-          group_2 << k
-        end
-      end
-      puts group_1
-      puts  "***********"
-      puts group_2
-      puts  "***********"
-      puts group_3
-      puts  "***********"
-      puts group_4
-      total = self.book_group_price(group_1) + self.book_group_price(group_2) +
-        self.book_group_price(group_3) + self.book_group_price(group_4)
+      groups = group_by_maximum_discount
+      total = book_group_price(groups[0]) + book_group_price(groups[1]) +
+        book_group_price(groups[2]) + book_group_price(groups[3])
     end
 
     total
@@ -55,7 +27,51 @@ class BookStore
 
   private
 
-  def self.book_group_price(group)
+  def group_by_maximum_discount
+    group_1 = []
+    group_2 = []
+    group_3 = []
+    group_4 = []
+    switcher = false
+    book_counts.each do |k, v|
+      if v == 2
+        if group_1.length > 3 && books.length > 12
+          group_3 << k
+          group_4 << k
+        else
+          group_1 << k
+          group_2 << k
+        end
+      elsif v == 3
+        group_1 << k
+        group_2 << k
+        group_3 << k
+      elsif v == 4
+        group_1 << k
+        group_2 << k
+        group_3 << k
+        group_4 << k
+      elsif switcher == false
+        group_1 << k
+        switcher = books.length != 6
+      elsif switcher == true
+        group_2 << k
+      end
+    end
+    groups = [group_1, group_2, group_3, group_4]
+  end
+
+  def number_of_groups
+    (books.length / 5.0).ceil
+  end
+
+  def book_counts
+    book_counts = Hash.new(0)
+    books.each { |book| book_counts[book] += 1 }
+    book_counts
+  end
+
+  def book_group_price(group)
     case group.uniq.length
     when 0..1
       group.length * 8
