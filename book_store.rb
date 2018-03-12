e7eeae423673398cb5b4989a2377f058b210d957
c1505 <coreym247@gmail.com>
2018-03-12 20:24:04 +0000

    extracted group class.  will allow for easier extraction of methods

--- book_store.rb
+++ book_store.rb
@@ -30,40 +30,11 @@ class BookStore
 
 
   
-  class Groups
-    attr_reader :book_counts, :groups, :group_index, :book
-    
-    def initialize(book_counts)
-      @book_counts = book_counts.to_a
-    end
-  end
+  
   
   def group_books(max_group_size)
-    book_counts_array = book_counts.to_a
-    groups = [[]]
-    group_index = 0
-    until book_counts_array.empty?
-      book_counts_array = book_counts_array.map do |book, quantity|
-        if quantity > 0
-          if book_included_in_current_group?(groups, group_index, book) && groups.count > 1
-            unless any_groups_exist_excluding_current_book?(groups, book)
-              group_index += 1
-            end
-            groups = swap_book_with_other_group(book, groups)
-          elsif book_excluded_in_current_group?(groups, group_index, book) && max_group_size_exceeded?(groups, group_index, max_group_size)
-            groups[group_index] << book
-          else # groups[group_index].length == max_group_size # or group is one or less and book is already in the group
-            group_index += 1          #increment group
-            groups[group_index] = []  #incrment group
-            
-            groups[group_index] << book
-          end
-          [book, (quantity - 1)]
-          
-        end
-      end.compact
-    end
-    groups
+    group = Group.new(book_counts, max_group_size)
+    group.group_books
   end
   
   def book_included_in_current_group?(groups, group_index, book)
@@ -133,6 +104,81 @@ class BookStore
     end
   end
 end
+
+class Group
+    attr_reader :book_counts, :groups, :group_index, :book, :max_group_size
+    
+    def initialize(book_counts, max_group_size)
+      @book_counts = book_counts.to_a
+      @max_group_size = max_group_size
+    end
+    
+    def group_books
+      book_counts_array = book_counts.to_a
+      groups = [[]]
+      group_index = 0
+      until book_counts_array.empty?
+        book_counts_array = book_counts_array.map do |book, quantity|
+          if quantity > 0
+            
+            if book_included_in_current_group?(groups, group_index, book) && groups.count > 1
+              unless any_groups_exist_excluding_current_book?(groups, book)
+                group_index += 1
+              end
+              groups = swap_book_with_other_group(book, groups)
+            elsif book_excluded_in_current_group?(groups, group_index, book) && max_group_size_exceeded?(groups, group_index, max_group_size)
+              groups[group_index] << book
+            else # groups[group_index].length == max_group_size # or group is one or less and book is already in the group
+              group_index += 1          #increment group
+              groups[group_index] = []  #incrment group
+              
+              groups[group_index] << book
+            end
+            [book, (quantity - 1)]
+          end
+        end.compact
+      end
+      groups
+    end
+    
+    private
+    
+    def book_included_in_current_group?(groups, group_index, book)
+     groups[group_index].include?(book)
+    end
+  
+    def book_excluded_in_current_group?(groups, group_index, book)
+      !groups[group_index].include?(book)
+    end
+    
+    def any_groups_exist_excluding_current_book?(groups, book)
+      groups.select {|f| !f.include?(book) }.first
+    end
+    
+    def max_group_size_exceeded?(groups, group_index, max_group_size)
+      groups[group_index].length < max_group_size
+    end
+  
+    def swap_book_with_other_group(book, groups)
+      last_group = groups.last
+      other_groups = groups.select {|f| f.include?(book) }
+      swap_groups = groups.select {|f| !f.include?(book) }
+      swap_group = swap_groups.first
+      if swap_group
+        both_groups = last_group + swap_group + [book]
+        arr_1 = []
+        arr_2 = []
+        both_groups.sort.each_slice(2) do |f|
+          arr_1 << f[0]
+          arr_2 << f[1]
+        end
+        other_groups[0..-2] << arr_1 << arr_2
+      else
+        groups << [book]
+        groups
+      end
+    end
+  end
 
 module BookKeeping
   VERSION = 0
