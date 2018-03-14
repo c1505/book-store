ac7c1b54528bb512c886030a0d2fb3059b81cc49
c1505 <coreym247@gmail.com>
2018-03-14 02:04:52 +0000

    using instance variables, reduced number of parameters passed into methods

--- book_store.rb
+++ book_store.rb
@@ -19,59 +19,24 @@ class BookStore
     elsif number_of_groups > 1
       if total_price(group_books(4)) < total_price(group_books(5))
         total = total_price(group_books(4))
+        print group_books(4)
+        print total
+        print "***"
       else
         total = total_price(group_books(5))
+        print group_books(5)
       end
     end
     total
   end
 
   private
-
-
-  
-  
   
   def group_books(max_group_size)
     group = Group.new(book_counts, max_group_size)
     group.group_books
   end
   
-  def book_included_in_current_group?(groups, group_index, book)
-     groups[group_index].include?(book)
-  end
-  
-  def book_excluded_in_current_group?(groups, group_index, book)
-    !groups[group_index].include?(book)
-  end
-  
-  def any_groups_exist_excluding_current_book?(groups, book)
-    groups.select {|f| !f.include?(book) }.first
-  end
-  
-  def max_group_size_exceeded?(groups, group_index, max_group_size)
-    groups[group_index].length < max_group_size
-  end
-  
-  def swap_book_with_other_group(book, groups)
-    last_group = groups.last
-    other_groups = groups.select {|f| f.include?(book) }
-    swap_groups = groups.select {|f| !f.include?(book) }
-    swap_group = swap_groups.first
-    if swap_group
-      both_groups = last_group + swap_group + [book]
-      arr_1 = []
-      arr_2 = []
-      both_groups.sort.each_slice(2) do |f|
-        arr_1 << f[0]
-        arr_2 << f[1]
-      end
-      other_groups[0..-2] << arr_1 << arr_2
-    else
-      groups << [book]
-      groups
-    end
-  end
   
   def total_price(groups)
     groups.map do |group|
@@ -106,7 +71,7 @@ class BookStore
 end
 
 class Group
-    attr_reader :book_counts, :groups, :group_index, :book, :max_group_size
+    attr_reader :book_counts, :groups, :group_index, :max_group_size
     
     def initialize(book_counts, max_group_size)
       @book_counts = book_counts.to_a
@@ -114,25 +79,21 @@ class Group
     end
     
     def group_books
-      book_counts_array = book_counts.to_a
-      groups = [[]]
-      group_index = 0
-      until book_counts_array.empty?
-        book_counts_array = book_counts_array.map do |book, quantity|
+      @groups = [[]]
+      @group_index = 0
+      until book_counts.empty?
+        @book_counts = book_counts.map do |book, quantity|
           if quantity > 0
-            
-            if book_included_in_current_group?(groups, group_index, book) && groups.count > 1
-              unless any_groups_exist_excluding_current_book?(groups, book)
-                group_index += 1
+            if book_included_in_current_group?(book) && @groups.count > 1
+              unless any_groups_exist_excluding_current_book?(book)
+                @group_index += 1
               end
-              groups = swap_book_with_other_group(book, groups)
-            elsif book_excluded_in_current_group?(groups, group_index, book) && max_group_size_exceeded?(groups, group_index, max_group_size)
-              groups[group_index] << book
+              groups = swap_book_with_other_group(book)
+            elsif book_excluded_in_current_group?(book) && max_group_size_exceeded?
+              @groups[group_index] << book
             else # groups[group_index].length == max_group_size # or group is one or less and book is already in the group
-              group_index += 1          #increment group
-              groups[group_index] = []  #incrment group
-              
-              groups[group_index] << book
+              increment_group
+              @groups[group_index] << book
             end
             [book, (quantity - 1)]
           end
@@ -143,23 +104,28 @@ class Group
     
     private
     
-    def book_included_in_current_group?(groups, group_index, book)
-     groups[group_index].include?(book)
+    def increment_group
+      @group_index += 1
+      @groups[group_index] = []
     end
   
-    def book_excluded_in_current_group?(groups, group_index, book)
-      !groups[group_index].include?(book)
-    end
-    
-    def any_groups_exist_excluding_current_book?(groups, book)
-      groups.select {|f| !f.include?(book) }.first
-    end
-    
-    def max_group_size_exceeded?(groups, group_index, max_group_size)
-      groups[group_index].length < max_group_size
+    def book_included_in_current_group?(book)
+      @groups[group_index].include?(book)
     end
   
-    def swap_book_with_other_group(book, groups)
+    def book_excluded_in_current_group?(book)
+      !@groups[group_index].include?(book)
+    end
+    
+    def any_groups_exist_excluding_current_book?(book)
+      @groups.select {|f| !f.include?(book) }.first
+    end
+    
+    def max_group_size_exceeded?
+      @groups[group_index].length < max_group_size
+    end
+  
+    def swap_book_with_other_group(book)
       last_group = groups.last
       other_groups = groups.select {|f| f.include?(book) }
       swap_groups = groups.select {|f| !f.include?(book) }
@@ -172,10 +138,10 @@ class Group
           arr_1 << f[0]
           arr_2 << f[1]
         end
-        other_groups[0..-2] << arr_1 << arr_2
+        @groups = other_groups[0..-2] << arr_1 << arr_2
       else
-        groups << [book]
-        groups
+        @groups << [book]
+        @groups
       end
     end
   end
